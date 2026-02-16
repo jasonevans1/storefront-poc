74ebdd9917e743c2250408b17bcd03b93c029d38
Joseph Lawhorn <jlawhorn@degdigital.com>
2026-02-16 15:52:17 -0600

    Updated account page styles

--- blocks/commerce-account-header/commerce-account-header.js
+++ blocks/commerce-account-header/commerce-account-header.js
@@ -3,7 +3,7 @@ import { readBlockConfig } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const {
-    title = 'My account',
+    title = 'My Account',
   } = readBlockConfig(block);
 
   block.innerHTML = '';
